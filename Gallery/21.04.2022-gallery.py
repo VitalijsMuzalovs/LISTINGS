3400afa727c632cda6a3e8a44ba37e6e965ded62
Vitalijs Muzalovs <vm@ssbsia.lv>
2022-04-24 20:16:17 +0300

    Upd

--- Gallery/21.04.2022-gallery.py
+++ Gallery/21.04.2022-gallery.py
@@ -3,41 +3,6 @@ from PIL import ImageTk, Image
 
 win=Tk()
 
-def fwClick(img_num):
-    global vieta
-    global back,forward
-    # vieta=Label(image=imgList[img_num-1])
-    vieta.grid_forget()
-    vieta=Label(image=imgList[img_num+1])
-
-    
-
-    if img_num==5:
-        forward=Button(win,text=">>",state=DISABLED)
-
-    forward=Button(win,text=">>",command=lambda:fwClick(img_num+1))
-    back=Button(win,text="<<",command=lambda:bkClick(img_num-1))
-
-    vieta.grid(row=0,column=0,columnspan=3)
-    back.grid(row=1,column=0)
-    forward.grid(row=1,column=2)
-
-
-def bkClick(img_num):
-    global vieta
-    global back,forward
-    vieta.grid_forget()
-    vieta=Label(image=imgList[img_num-1])
-    forward=Button(win,text=">>",command=lambda:fwClick(img_num+1))
-    back=Button(win,text="<<",command=lambda:bkClick(img_num-1))
-
-    if img_num==1:
-        forward=Button(win,text="<<",state=DISABLED)
-    vieta.grid(row=0,column=0,columnspan=3)
-    back.grid(row=1,column=0)
-    forward.grid(row=1,column=2)
-
-
 img1=ImageTk.PhotoImage(Image.open("Gallery\img\Logo1.jpg"))
 img2=ImageTk.PhotoImage(Image.open("Gallery\img\Logo2.jpg"))
 img3=ImageTk.PhotoImage(Image.open("Gallery\img\Logo3.jpg"))
@@ -45,17 +10,48 @@ img4=ImageTk.PhotoImage(Image.open("Gallery\img\Logo4.jpg"))
 img5=ImageTk.PhotoImage(Image.open("Gallery\img\Logo5.jpg"))
 
 imgList = [img1,img2,img3,img4,img5]
-imgList[2]
+imgList[1]
 
 vieta=Label(image=img1)
-back=Button(win,text="<<",command=lambda:bkClick(1))
-forward=Button(win,text=">>",command=lambda:fwClick(1))
+vieta.grid(row=0,column=0,columnspan=3)
+
+back=Button(win,text="<<",state=DISABLED,command=lambda:bkClick())
+forward=Button(win,text=">>",command=lambda:fwClick(2))
 beigt=Button(win,text="Close",command=win.quit)
 
-vieta.grid(row=0,column=0,columnspan=3)
 beigt.grid(row=1,column=1)
-
 back.grid(row=1,column=0)
 forward.grid(row=1,column=2)
+
+def fwClick(img_num):
+    global vieta
+    global back,forward
+
+    forward=Button(win,text=">>",command=lambda:fwClick(img_num+1))
+    if img_num==len(imgList):forward=Button(win,text=">>",state=DISABLED)
+    back=Button(win,text="<<",command=lambda:bkClick(img_num-1))
+    vieta.grid_forget()
+    vieta=Label(image=imgList[img_num-1])
+
+    vieta.grid(row=0,column=0,columnspan=3)
+    back.grid(row=1,column=0)
+    forward.grid(row=1,column=2)
+    
+
+
+def bkClick(img_num):
+    global vieta
+    global back,forward  
+
+    forward=Button(win,text=">>",command=lambda:fwClick(img_num+1))
+    back=Button(win,text="<<",command=lambda:bkClick(img_num-1))
+    if img_num==1:back=Button(win,text="<<",state=DISABLED)
+    print(img_num)
+    vieta.grid_forget()
+    vieta=Label(image=imgList[img_num-1])
+    
+    vieta.grid(row=0,column=0,columnspan=3)
+    back.grid(row=1,column=0)
+    forward.grid(row=1,column=2)
 
 win.mainloop()
