91cb99a8b3ab5790fb1fa1a1fb1cbe4fda19e7bb
Vitalijs Muzalovs <vm@ssbsia.lv>
2022-05-01 20:42:16 +0300

    upd

--- 29.04.2022-lekcija/rdVeikals.py
+++ 29.04.2022-lekcija/rdVeikals.py
@@ -1,6 +1,9 @@
 import requests
 from bs4 import BeautifulSoup
-import lxml
+from operator import itemgetter
+
+def fieldMaxLen(fieldNum):
+    return len(max(outputToFile, key=lambda x:len(x[fieldNum]))[fieldNum])
 
 myURL='https://www.rdveikals.lv/categories/ru/216/sort/5/filter/0_0_0_0/page/1/%D0%A5%D0%BE%D0%BB%D0%BE%D0%B4%D0%B8%D0%BB%D1%8C%D0%BD%D0%B8%D0%BA%D0%B8.html'
 myDomain=myURL[myURL.find('//')+len('//'):]
@@ -22,7 +25,7 @@ for item in pageLinksStr:
     pageLinks.append(myLink)
 
 i=0
-writeToFile,outputToFile='',''
+writeToFile,outputToFile='',[]
 
 for link in pageLinks:
     response = requests.get(link).text
@@ -33,11 +36,19 @@ for link in pageLinks:
         itemTitle=k.find('h3',class_='product__title').text.strip()
         itemName=list(itemTitle.split('\n'))[0]
         itemBrand=(list(itemTitle.split('\n'))[1]).strip()
-        itemPrice=k.find('p',class_='price').text.strip()
+        itemPrice=float(k.find('p',class_='price').text.strip('€'))
         outputString=str(f'{i}.{itemBrand} - {itemName}: {itemPrice}')
-        outputToFile=outputToFile+outputString+'\n'
+        outputToFile.append((itemBrand,itemName,itemPrice))
         print(outputString)
-            
-    with open(fr'E:\TEHNIKUMS\Python\29.04.2022-lekcija\RDveikals-Fridges\Fridges.txt', 'w') as f:
-        f.write(outputToFile)
-        f.close
+    
+# SORT
+outputToFile=sorted(outputToFile,key=itemgetter(0,2),reverse=True)
+
+BrandMaxLen=fieldMaxLen(0)
+NameMaxLen=fieldMaxLen(1)
+
+with open(fr'E:\TEHNIKUMS\Python\29.04.2022-lekcija\RDveikals-Fridges\Fridges.txt', 'w') as f:
+    for num,line in enumerate(outputToFile):
+        f.write(str(num+1)+'.'+' '*(5-len(str(num+1)))+line[0]+' '*(BrandMaxLen+2-len(line[0])) + line[1]+':'+' '*(NameMaxLen+2-len(line[1]))+str(line[2])+' €'+'\n')
+    f.close
+
