ac3921bcacfc3c548b5be1e8b351374b920d0c9a
Vitalijs Muzalovs <vm@ssbsia.lv>
2022-05-01 14:24:50 +0300

    upd

--- 29.04.2022-lekcija/rdVeikals.py
+++ 29.04.2022-lekcija/rdVeikals.py
@@ -16,14 +16,12 @@ pageLinksStr=str(pages[0].find_all('option')).split(',')
 pageLinks=[]
 
 for item in pageLinksStr:
-        # print(str(item)+'\n')
-        startOfStr= item.find('value="')+len('value="')
-        endOfStr=(item.find('html')+len('html'))
-        myLink='https://'+myDomain+'/'+item[startOfStr:endOfStr]
-        pageLinks.append(myLink)
-        # print(myLink)
+    startOfStr= item.find('value="')+len('value="')
+    endOfStr=(item.find('html')+len('html'))
+    myLink='https://'+myDomain+'/'+item[startOfStr:endOfStr]
+    pageLinks.append(myLink)
 
-i=1
+i=0
 writeToFile=''
 
 for link in pageLinks:
@@ -31,15 +29,15 @@ for link in pageLinks:
     soup = BeautifulSoup(response, 'lxml')
     items = soup.find_all('li',class_='col col--xs-4 product js-product js-touch-hover')
     
-    for i,k in enumerate(items,start=i):
+    for i,k in enumerate(items,start=i+1):
         itemTitle=k.find('h3',class_='product__title').text.strip()
         itemName=list(itemTitle.split('\n'))[0]
         itemBrand=(list(itemTitle.split('\n'))[1]).strip()
         itemPrice=k.find('p',class_='price').text.strip()
-        writeString=str(f'{i}.{itemBrand} - {itemName}: {itemPrice}')
-        writeToFile=writeToFile+str(f'{i}.{itemBrand} - {itemName}: {itemPrice}\n')
-        print(writeString)
+        outputString=str(f'{i}.{itemBrand} - {itemName}: {itemPrice}')
+        outputToFile=writeToFile+str(f'{i}.{itemBrand} - {itemName}: {itemPrice}\n')
+        print(outputString)
             
     with open(fr'E:\TEHNIKUMS\Python\29.04.2022-lekcija\RDveikals-Fridges\Fridges.txt', 'w') as f:
-        f.write(writeToFile)
+        f.write(outputToFile)
         f.close
