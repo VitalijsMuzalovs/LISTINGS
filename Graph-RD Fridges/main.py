651352309ec19b23a047b694cbeba4fe087ae9cd
Vitalijs Muzalovs <vm@ssbsia.lv>
2022-05-15 20:52:02 +0300

    upd

--- Graph-RD Fridges/main.py
+++ Graph-RD Fridges/main.py
@@ -1,36 +1,53 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-fileDir="2022.04.22-RD_parser\RDveikals-Fridges\Fridges.txt"
+fileDir="2022.04.22-RD_parser\RDveikals-Fridges\Fridges_REF.txt" #REFERENCE FILE
 
 brand=[]
 model=[]
 price=[]
+ref={}
 
-with open(fileDir,'r') as file:
-    for line in file:
-        # print(line)
-        tmp=line.split(' ')
-        tmp=[el for el in tmp if el]
-        brand.append(tmp[1])
+def getRefLists(fileDir):
+    with open(fileDir,'r') as file:
+        for line in file:
+            # print(line)
+            tmp=line.split(' ')
+            tmp=[el for el in tmp if el]
+            brand.append(tmp[1])
 
-        price.append(tmp[-2])
-        model.append((' '.join(tmp[2:-2]))[:-1])
-        
+            price.append(tmp[-2])
+            model.append((' '.join(tmp[2:-2]))[:-1])
+    return model,brand,price
+
+model,brand,price=getRefLists(fileDir)
 for n in range(len(brand)):
-    print(f'{brand[n]} - {model[n]} - {price[n]}')
+    # print(f'{brand[n]} - {model[n]} - {price[n]}')
+    ref={model[n]:{'brand':brand[n],'price_ref':price[n]}}
+    print(ref)
 
+fileDir="2022.04.22-RD_parser\RDveikals-Fridges\Fridges13.05.2022.txt"
+model,brand,price=getRefLists(fileDir)
+for n in model:
+    if n in ref:
+        ref[n]={'price_1':price[n]}
+        print(ref[n])
 
+# for key, value in ref.items():
+#     print(key, value)
+#     print('='*30)
 
-fig, ax = plt.subplots()
-for color in ['tab:blue', 'tab:orange', 'tab:green']:
-    n = 750
-    x, y = np.random.rand(2, n)
-    scale = 200.0 * np.random.rand(n)
-    ax.scatter(x, y, c=color, s=scale, label=color,
-               alpha=0.3, edgecolors='none')
+# print(ref['CU3331'])
 
-ax.legend()
-ax.grid(True)
+# fig, ax = plt.subplots()
+# for color in ['tab:blue', 'tab:orange', 'tab:green']:
+#     n = 750
+#     x, y = np.random.rand(2, n)
+#     scale = 200.0 * np.random.rand(n)
+#     ax.scatter(x, y, c=color, s=scale, label=color,
+#                alpha=0.3, edgecolors='none')
 
-plt.show()
+# ax.legend()
+# ax.grid(True)
+
+# plt.show()
