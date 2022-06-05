204bb3d2c15dcb1d3150e44582838621fe5b8aed
VM <you@example.com>
2022-06-05 16:07:26 +0300

    upd

--- test.py
+++ test.py
@@ -1,3 +1,20 @@
-a=[]
-a= [x+5 for x in range(0,5)]
-print(a)
+from tkinter import Frame, Tk
+
+class MyApp():
+    def __init__(self):
+        self.root = Tk()
+
+        self.my_frame_red = Frame(self.root, bg='red',width=150)
+        self.my_frame_red.grid(row=0, column=0, sticky='nsew')
+
+        self.my_frame_blue = Frame(self.root, bg='blue',width=250)
+        self.my_frame_blue.grid(row=0, column=1, sticky='nsew')
+
+        self.root.grid_rowconfigure(0, minsize=20, weight=1)
+        self.root.grid_columnconfigure(0, minsize=20, weight=1)
+        self.root.grid_columnconfigure(1, weight=1)
+
+        self.root.mainloop()
+
+if __name__ == '__main__':
+    app = MyApp()
