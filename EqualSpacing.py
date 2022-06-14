00f42730a1767ff517bc6f88036a0b605c6adba8
VitalMartell <55775289+VitalMartell@users.noreply.github.com>
2022-06-14 11:55:19 +0300

    panels

--- EqualSpacing.py
+++ EqualSpacing.py
@@ -1,17 +1,25 @@
+"""
+This module calculates grooving width,distance between groovings and length of side's grooving (leftovers).
+Conditions: 
+- 2 panels with different length with mirrored (symmetric from panel center) groovings on them.
+- panel's ends up with equal groovings (within tolerance 'side_tolerance')
+
+"""
+from typing import Type
 import numpy as np
 
 hill_min=2
 hill_max=30
 hill_step=0.1
 groove_min=6
-groove_max=20
+groove_max=25
 groove_step=0.1
 panel_1=1280
 panel_2=1180
 
+side_tolerance=1
 
-
-def sides(whole_len,hill,groove):
+def sides(whole_len,groove,hill):
     count = int((whole_len-hill)/(hill+groove)*2+1)
     if count%2!=0:
         odd_count=count
@@ -23,23 +31,17 @@ def sides(whole_len,hill,groove):
             return sides
 
 
-
-# def sides(panel_len,step):
-#     count = int(panel_len//step)
-#     if count%2!=0:
-#         odd_count=count
-#         check_sim = (odd_count-5)%4
-#         if check_sim==0:
-#             sides_leftover = step*odd_count
-#             sides=(panel_len-sides_leftover)/2
-#             print(f'Groove_width: {round(step,2)} \t Sides:{round(sides,2)} \t {panel_len}')
-#             return sides
+def leftover(whole_len,groove,hill):
+    odd_count = int((whole_len-hill)/(hill+groove)*2+1)
+    sides_leftover = (whole_len-(odd_count-1)/2*(hill+groove)-hill)/2
+    return odd_count
 
 for i in np.arange(groove_min,groove_max,groove_step):
     for k in np.arange(hill_min,hill_max,hill_step):
 
         side_1=sides(panel_1,i,k)
         side_2=sides(panel_2,i,k)
-        # print(f'Step:{round(i,2)}: {side_1} and {side_2}')
-        if not(side_1 is None) and not(side_2 is None) and abs(side_1-side_2)<=0.5 and side_1>i/2:
-            print(f'##### Groove width {round(i)}  hill width {round(k,2)} fits!##### Side is {round(side_1,2)}/{round(side_2,2)}')
+
+        if not(side_1 is None) and not(side_2 is None) and abs(side_1-side_2)<=side_tolerance and side_1>i/2:
+            print(f'Канавка: {round(i)}  Между канавок: {round(k,2)} Остается по краям: {round(side_1,2)}/{round(side_2,2)} Кол-во: {leftover(panel_1,i,k)}')
+
