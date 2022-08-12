f223a4d2feead5f26949c81ac2af1efa5987e6f7
VitalMartell <55775289+VitalMartell@users.noreply.github.com>
2022-08-12 08:31:19 +0300

    upd

--- test.py
+++ test.py
@@ -1,3 +1,17 @@
-a=None
-if a>2:
-    print('Ok')
+def collatz(n):
+    n = int(n)
+    lst = []
+    while n>1:
+        lst.append(n)
+        print(n)
+        if n%2 == 0: 
+            n=n/2
+        else:
+            n=n*3+1
+        if n==1: 
+            lst.append(n)
+            print(n)
+    print(len(lst))
+    return len(lst)
+
+collatz(73567465519280238573)
